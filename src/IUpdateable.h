162bdf246827505d9ab8f6f2a08d2bcc30f69c3f
mgdsstudio <mgdsstudio@yahoo.com>
2026-04-09 08:53:23 +0300

    Tablet

---
+++ src/IUpdateable.h
@@ -0,0 +1,16 @@
+//
+// Created by Alexander on 09.04.2026.
+//
+
+#ifndef CMAKESFMLPROJECT_IUPDATEABLE_H
+#define CMAKESFMLPROJECT_IUPDATEABLE_H
+
+
+class IUpdateable {
+public:
+    void update(float tpf);
+    void complete();
+};
+
+
+#endif //CMAKESFMLPROJECT_IUPDATEABLE_H
