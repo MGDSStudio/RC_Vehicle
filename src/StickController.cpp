2c4529975b5d3e2757b5b763107cb755485a8bca
MGDSStudio <mgdsstudio@yahoo.com>
2026-04-14 17:11:27 +0300

    no1

--- src/StickController.cpp
+++ src/StickController.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "StickController.h"
+
+void StickController::updateInDeadZone()
+{
+    float angleToTouchPlace = ;
+
+
+}
--- src/StickController.h
+++ src/StickController.h
@@ -4,12 +4,32 @@
 
 #ifndef CMAKESFMLPROJECT_STICKCONTROLLER_H
 #define CMAKESFMLPROJECT_STICKCONTROLLER_H
+#include "TouchPlace.h"
 
 
 class StickController
 {
 
+public:
+    void setX(const float x)
+    {
+        touch_place.setX(x);
+        updateInDeadZone();
+    }
 
+    void setY(const float x)
+    {
+        touch_place.setX(x);
+        updateInDeadZone();
+    }
+
+
+private:
+    static constexpr float DEAD_ZONE_ANGLE = 25;
+    TouchPlace touch_place;
+    bool inDeadZone = true;
+
+    void updateInDeadZone();
 };
 
 
